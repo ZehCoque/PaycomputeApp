1f6e0099a662e5549b96ac2fbcd9a725287b67d9
José Coque <jose.coque@hotmail.com>
2021-04-10 12:35:06 -0300

    feat: Index - EmployeeController

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Paycompute.Models;
+using Paycompute.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.Fullname,
+                Gender = employee.Gender,
+                Designation = employee.Designation,
+                City = employee.City,
+                DateJoined = employee.DataJoined
+            }).ToList(); //retrieve all employees from the database and project it to the View Model
+            return View(employees);
+        }
+    }
+}
---
+++ Paycompute/Models/EmployeeIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Models
+{
+    public class EmployeeIndexViewModel
+    {
+        //here goes what we would like to display to the controller
+        public int Id { get; set; }
+        public string EmployeeNo { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string Designation { get; set; }
+        public string City { get; set; }
+
+    }
+}
